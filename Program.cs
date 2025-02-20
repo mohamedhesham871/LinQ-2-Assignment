26b00804c7d1260cd8eb6adc3a829cde91a38370
20100 <mmmelkady23@gmail.com>
2025-02-20 18:49:33 +0200

    LINQ - Aggregate Operators [4]

--- Program.cs
+++ Program.cs
@@ -30,39 +30,42 @@ namespace LinQ_2_Assignment
 
             #region LINQ - Aggregate Operators
             #region 01 Uses Count to get the number of odd numbers in the array
-            int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
-            var res = Arr.Count(p => p % 2 == 1);
-            Console.WriteLine(res);
+            //int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var res = Arr.Count(p => p % 2 == 1);
+            //Console.WriteLine(res);
             #endregion
-            Console.WriteLine("----------");
             #region 02: Return a list of customers and how many orders each has.
-            var order = CustomerList.Select(p => new { customer = p, countofOrder = p.Orders.Count() });
+            // var order = CustomerList.Select(p => new { customer = p, countofOrder = p.Orders.Count() });
             //foreach (var i in order)
             //{
             //    Console.WriteLine(i);
             //}
             #endregion
-            Console.WriteLine("----------");
-
             #region 3 Return a list of categories and how many products each has
-            var cat= from n in ProductList
-                     group n by n.Category
-                     into g
-                     select new {name=g.Key ,count =g.Count() };
-            //-------------Another Solve--------
-            cat = ProductList.GroupBy(p => p.Category).Select((p) => new { name = p.Key, count = p.Count() });
+            //var cat= from n in ProductList
+            //         group n by n.Category
+            //         into g
+            //         select new {name=g.Key ,count =g.Count() };
+            ////-------------Another Solve--------
+            //cat = ProductList.GroupBy(p => p.Category).Select((p) => new { name = p.Key, count = p.Count() });
 
-            foreach(var i in cat)
-            {
-                Console.WriteLine(i);
-                Console.WriteLine("-----");
-            }
+            //foreach(var i in cat)
+            //{
+            //    Console.WriteLine(i);
+            //    Console.WriteLine("-----");
+            //}
+
+            #endregion
+            #region 4:4. Get the total of the numbers in an array.
+            int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+             var res=Arr.Count();
+            Console.WriteLine(res);//10 Element
+            res = Arr.Sum();
+            Console.WriteLine(res);//Sum of Element
 
             #endregion
             /*
-             1. 
-                2. 
-                3.
+           
             */
             #endregion
         }
