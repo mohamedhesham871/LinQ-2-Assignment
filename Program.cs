7678d82261592f257240e12241bcf512dd472a01
20100 <mmmelkady23@gmail.com>
2025-02-20 17:40:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using static LinQ_2_Assignment.ListGenerators;
+namespace LinQ_2_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region MyRegion
+
+            #endregion
+        }
+    }
+}
